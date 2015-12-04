249067fe36a7c70e0c3ad4849394bdb29f5df6b7
Rizzy <RizAhmed@users.noreply.github.com>
2015-12-03 22:05:34 -0800

    Finished Tutorial with First and Second Activity screens.

--- AppProject/app/src/main/java/ca/bcit/rizzy/MainActivity.java
+++ AppProject/app/src/main/java/ca/bcit/rizzy/MainActivity.java
@@ -1,9 +1,13 @@
 package ca.bcit.rizzy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+        public void onClick(View v){
+                goToSecondActivity();
+            }
+        });
+
+
     }
 
     @Override
@@ -34,4 +48,12 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void goToSecondActivity()
+    {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+
 }
